bdda865184a5c7801e9e01c9f965d64cabf42aef
IDK <liketoaccess@gmail.com>
2018-02-02 12:41:54 -0600

    Create permutions.rb

---
+++ permutions.rb
@@ -0,0 +1,12 @@
+def perm arr, i = 0;
+  p arr if i == arr.size
+  (i...arr.size).each do |j|
+    arr[i], arr[j] = arr[j], arr[i]
+    perm arr, i+1
+    arr[i], arr[j] = arr[j], arr[i]
+   end 
+end 
+puts "Welcome to algorithm_Find. This was maybe made by SIE (Warren Patal) and the S.A.T.O Project."
+puts "What is Your String?"
+numberInput = gets.chomp 
+perm numberInput
